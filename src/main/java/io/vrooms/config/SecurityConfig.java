8b77b14044a0ef5d312273c81f3772e085ea7472
Arthur Aukhatov <aaukhatov@gmail.com>
2020-04-30 22:40:47 +0300

    Allow to access /swagger-ui/** resources

--- src/main/java/io/vrooms/config/SecurityConfig.java
+++ src/main/java/io/vrooms/config/SecurityConfig.java
@@ -12,8 +12,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	public static final String API_DOCS = "/api-docs";
+	public static final String API_DOCS = "/api-docs/**";
 	public static final String SWAGGER_UI = "/swagger-ui.html";
+	public static final String SWAGGER_UI_RESOURCES = "/swagger-ui/**";
 
 	private final OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuthUserService;
 
@@ -25,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers(API_DOCS, SWAGGER_UI).anonymous()
+				.antMatchers(API_DOCS, SWAGGER_UI, SWAGGER_UI_RESOURCES).permitAll()
 				.and()
 				.authorizeRequests()
 				.anyRequest().authenticated()
